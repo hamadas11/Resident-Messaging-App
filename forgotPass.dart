853de632d4a421664e8eaeb520a0902af35e16ae
hamadas11 <118951694+hamadas11@users.noreply.github.com>
2023-06-27 18:01:09 +0300

    Create forgotPass.dart

---
+++ forgotPass.dart
@@ -0,0 +1,255 @@
+import '/auth/firebase_auth/auth_util.dart';
+import '/flutter_flow/flutter_flow_icon_button.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+import 'forgot_password_model.dart';
+export 'forgot_password_model.dart';
+
+class ForgotPasswordWidget extends StatefulWidget {
+  const ForgotPasswordWidget({Key? key}) : super(key: key);
+
+  @override
+  _ForgotPasswordWidgetState createState() => _ForgotPasswordWidgetState();
+}
+
+class _ForgotPasswordWidgetState extends State<ForgotPasswordWidget> {
+  late ForgotPasswordModel _model;
+
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => ForgotPasswordModel());
+
+    _model.emailAddressController ??= TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _model.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    context.watch<FFAppState>();
+
+    return Scaffold(
+      key: scaffoldKey,
+      backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(100),
+        child: AppBar(
+          backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
+          automaticallyImplyLeading: false,
+          title: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 8),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    FlutterFlowIconButton(
+                      borderColor: Colors.transparent,
+                      borderRadius: 30,
+                      borderWidth: 1,
+                      buttonSize: 50,
+                      icon: Icon(
+                        Icons.arrow_back_rounded,
+                        color: FlutterFlowTheme.of(context).primaryText,
+                        size: 24,
+                      ),
+                      onPressed: () async {
+                        context.pop();
+                      },
+                    ),
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(4, 0, 0, 0),
+                      child: Text(
+                        FFLocalizations.of(context).getText(
+                          '3b1db2nm' /* Revenir */,
+                        ),
+                        style:
+                            FlutterFlowTheme.of(context).displaySmall.override(
+                                  fontFamily: 'Poppins',
+                                  fontSize: 16,
+                                ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          actions: [],
+          centerTitle: true,
+          elevation: 0,
+        ),
+      ),
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(20, 0, 0, 0),
+                child: Text(
+                  FFLocalizations.of(context).getText(
+                    'uhliwibw' /* Mot de passe */,
+                  ),
+                  style: FlutterFlowTheme.of(context).bodyMedium.override(
+                        fontFamily: 'Jost',
+                        fontSize: 50,
+                      ),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(20, 4, 20, 8),
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(4, 4, 4, 4),
+                    child: Text(
+                      FFLocalizations.of(context).getText(
+                        'ehhau3lc' /* Nous vous enverrons un e-mail ... */,
+                      ),
+                      style: FlutterFlowTheme.of(context).bodySmall,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(24, 4, 24, 0),
+            child: Container(
+              width: double.infinity,
+              height: 60,
+              decoration: BoxDecoration(
+                color: FlutterFlowTheme.of(context).secondaryBackground,
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 5,
+                    color: Color(0x4D101213),
+                    offset: Offset(0, 2),
+                  )
+                ],
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: TextFormField(
+                controller: _model.emailAddressController,
+                obscureText: false,
+                decoration: InputDecoration(
+                  labelText: FFLocalizations.of(context).getText(
+                    'bnni76nm' /* Votre adresse e-mail... */,
+                  ),
+                  labelStyle: FlutterFlowTheme.of(context).bodySmall,
+                  hintText: FFLocalizations.of(context).getText(
+                    '91fpnpjm' /* Entrer votre Email... */,
+                  ),
+                  hintStyle: FlutterFlowTheme.of(context).bodyMedium.override(
+                        fontFamily: 'Lexend Deca',
+                        color: Color(0xFF57636C),
+                        fontSize: 14,
+                        fontWeight: FontWeight.normal,
+                      ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(0x00000000),
+                      width: 0,
+                    ),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(0x00000000),
+                      width: 0,
+                    ),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  errorBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(0x00000000),
+                      width: 0,
+                    ),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  focusedErrorBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(0x00000000),
+                      width: 0,
+                    ),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  filled: true,
+                  fillColor: FlutterFlowTheme.of(context).secondaryBackground,
+                  contentPadding:
+                      EdgeInsetsDirectional.fromSTEB(24, 24, 20, 24),
+                ),
+                style: FlutterFlowTheme.of(context).bodyMedium,
+                maxLines: null,
+                validator:
+                    _model.emailAddressControllerValidator.asValidator(context),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(0, 40, 0, 0),
+            child: FFButtonWidget(
+              onPressed: () async {
+                if (_model.emailAddressController.text.isEmpty) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text(
+                        'Email required!',
+                      ),
+                    ),
+                  );
+                  return;
+                }
+                await authManager.resetPassword(
+                  email: _model.emailAddressController.text,
+                  context: context,
+                );
+              },
+              text: FFLocalizations.of(context).getText(
+                'hdgijitu' /* Envoyer un lien */,
+              ),
+              options: FFButtonOptions(
+                width: 270,
+                height: 50,
+                padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+                iconPadding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+                color: FlutterFlowTheme.of(context).primaryText,
+                textStyle: FlutterFlowTheme.of(context).titleSmall.override(
+                      fontFamily: 'Poppins',
+                      color: FlutterFlowTheme.of(context).secondaryBackground,
+                    ),
+                elevation: 3,
+                borderSide: BorderSide(
+                  color: Colors.transparent,
+                  width: 1,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
